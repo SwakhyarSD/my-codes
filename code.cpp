b4c3266d8dbeadbb0169c0eb9fa49b77b0afa3df
SwakhyarSD <swakhyarsauravdas@gmail.com>
2025-05-30 22:21:25 +0530

    new

---
+++ code.cpp
@@ -0,0 +1,10 @@
+// g++ code.cpp -o a.exe; ./a.exe
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y, z;
+    cin >> x >> y >> z;
+    cout << (char)x << (char)y << (char)z;
+}
